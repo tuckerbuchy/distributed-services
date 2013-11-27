13650319d6cb6b95f995ab770e1c8cafe8193df6
tuckerbuchy <tucker.buchy@gmail.com>
2013-11-27 14:55:09 -0800

    added client input

--- client.py
+++ client.py
@@ -21,13 +21,19 @@ class Client:
 		service_request = json.dumps(service_request_json)
 		socket.send(service_request)
 		services = socket.recv()
-		print "Recieved these services:"
-		print services
+		print "Recieved these services: \n\t\t%s" %(services)
+		self.startPrompt()
 	
 	def connectToServer(self):
 		socket = self.context.socket(zmq.REQ)
 		socket.connect (SERVER_ADDRESS)
 		return socket
+
+	def startPrompt(self):
+		service_id = raw_input("Enter service you want to call:")
+		args = raw_input("Enter args, deliminated by comma (ex. 1,2,3):")
+		#convert the input to an array
+		args_array = args.split(',')
 
 client = Client()
 client.getServices()
