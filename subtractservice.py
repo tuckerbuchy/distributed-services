6c2d5bd7c9a4fa598224c78c2419abb23b9a532a
tuckerbuchy <tucker.buchy@gmail.com>
2013-11-30 14:11:15 -0800

    deleted old files|

--- subtractservice.py
+++
@@ -1,8 +0,0 @@
-from service import *
-
-printString = lambda x: x
-
-service = ServiceProvidingNode()
-service.addService(Service("printString", printString))
-service.connectToRegistryServer(SERVER_ADDRESS)
-service.registerServices()
